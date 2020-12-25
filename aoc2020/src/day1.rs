b79aee22b1e647f43646116dbb7069d894a9ec52
liftnhoff <45518408+liftnhoff@users.noreply.github.com>
2020-12-24 19:52:23 -0500

    starting to get rust figured out

--- aoc2020/src/day1.rs
+++ aoc2020/src/day1.rs
@@ -1,4 +1,3 @@
 pub fn part1() {
-    println!("Hello, world day 1!");
+    
 }
-
--- aoc2020/src/main.rs
+++ aoc2020/src/main.rs
@@ -1,5 +1,21 @@
-use aoc2020::day1;
+mod day1;
+mod utils;
+
+use std::env;
 
 fn main() {
-    day1::part1();    
+    let args: Vec<String> = env::args().collect();
+    let day = &args[1];
+    let input_file = &args[2];
+
+    if day == "day1" {
+        println!("made it here");
+        let values = utils::parse_file_of_ints("input_data/example_day1.txt");
+        for value in values {
+            println!("{}", value.to_string());
+        }
+        day1::part1();
+    }
+
+    
 }
