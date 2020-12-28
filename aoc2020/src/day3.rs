46446906be12fa1862237a8fcc6208a9e7073f48
liftnhoff <45518408+liftnhoff@users.noreply.github.com>
2020-12-28 16:36:20 -0500

    day 3 part 2

--- aoc2020/src/day3.rs
+++ aoc2020/src/day3.rs
@@ -3,8 +3,8 @@ use std::fs;
 pub fn run(input_file: &str) {
     let tree_map = parse_day3_input_file(input_file).unwrap();
     // tree_map.print();
-    println!("part 1:  {:?}", part1(tree_map));
-    // println!("part 2:  {:?}", part2(&values));
+    println!("part 1:  {:?}", part1(&tree_map));
+    println!("part 2:  {:?}", part2(&tree_map));
 }
 
 fn parse_day3_input_file(file_name: &str) -> Result<TreeMap, std::io::Error> {
@@ -31,7 +31,7 @@ fn parse_day3_input_file(file_name: &str) -> Result<TreeMap, std::io::Error> {
     return Ok(tree_map);
 }
 
-fn part1(tree_map: TreeMap) -> u32 {
+fn part1(tree_map: &TreeMap) -> u32 {
     let steps_right_per_row = 3;
     let mut tree_count: u32 = 0;
     let mut col_index = 0;
@@ -44,9 +44,43 @@ fn part1(tree_map: TreeMap) -> u32 {
     return tree_count;
 }
 
-// fn part2(passwords_and_policies: &Vec<PasswordAndPolicy>) -> u32 {
+fn part2(tree_map: &TreeMap) -> u64 {
+    let steps_right_per_row = [1, 3, 5, 7];
+    let mut tree_count_per_row = [0, 0, 0, 0];
+    let mut col_indices_per_row = [0, 0, 0, 0];
 
-// }
+    let steps_right_per_two_rows = [1];
+    let mut tree_count_per_two_rows = [0];
+    let mut col_indices_per_two_rows = [0];
+
+    for (row_index, tree_row) in tree_map.rows.iter().enumerate() {
+        for count_index in 0..tree_count_per_row.len() {
+            if tree_row.has_tree(col_indices_per_row[count_index]) {
+                tree_count_per_row[count_index] += 1;
+            }
+            col_indices_per_row[count_index] += steps_right_per_row[count_index];
+        }
+
+        if row_index % 2 == 0 {
+            for count_index in 0..tree_count_per_two_rows.len() {
+                if tree_row.has_tree(col_indices_per_two_rows[count_index]) {
+                    tree_count_per_two_rows[count_index] += 1;
+                }
+                col_indices_per_two_rows[count_index] += steps_right_per_two_rows[count_index];
+            }
+        }
+    }
+
+    let mut result: u64 = 1;
+    for value in &tree_count_per_row {
+        result *= value;
+    }
+    for value in &tree_count_per_two_rows {
+        result *= value;
+    }
+
+    return result;
+}
 
 struct TreeMap {
     rows: Vec<TreeRow>,
