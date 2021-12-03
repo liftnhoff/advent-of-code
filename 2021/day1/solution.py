d3132d741a3fcefdc0993817710d196c5154800f
liftnhoff <45518408+liftnhoff@users.noreply.github.com>
2021-12-02 20:05:44 -0500

    2021 day 1 part 1

--- 2021/day1/solution.py
+++ 2021/day1/solution.py
@@ -3,7 +3,15 @@ from base.solution import AdventOfCodeSolution
 
 class Solution(AdventOfCodeSolution):
     def part1(self):
-        pass
+        step_increase_count = 0
+        current = int(self.input_data[0])
+        for index in range(1, len(self.input_data)):
+            next = int(self.input_data[index])
+            if next > current:
+                step_increase_count += 1
+            current = next
+
+        print(f"part 1: {step_increase_count}")
 
     def part2(self):
         pass
