10c844a6e7ea24640e618488573f21dc7d2589f6
liftnhoff <45518408+liftnhoff@users.noreply.github.com>
2021-12-16 20:53:34 -0500

    day 9 test input works, main doesn't

---
+++ 2021/day9/solution.py
@@ -0,0 +1,67 @@
+from dataclasses import dataclass
+from typing import Union
+
+from base.solution import AdventOfCodeSolutionBase
+
+
+@dataclass
+class Point:
+    row_index: int
+    col_index: int
+
+
+class HeightMap:
+    _RISK_CONSTANT = 1
+
+    def __init__(self, heights: tuple[tuple[int]]):
+        self._heights = heights
+
+        self._local_minima: list[Point] = []
+
+    def calculate_risk_level(self) -> int:
+        risk = 0
+        for point in self.local_minima:
+            risk += self._RISK_CONSTANT + self.get_height(
+                point.row_index, point.col_index
+            )
+
+        return risk
+
+    @property
+    def local_minima(self) -> list[Point]:
+        if self._local_minima:
+            return self._local_minima
+
+        for row_index, row in enumerate(self._heights):
+            for col_index in range(len(row)):
+                if self._is_point_local_minimum(row_index, col_index):
+                    self._local_minima.append(Point(row_index, col_index))
+
+        return self._local_minima
+
+    def _is_point_local_minimum(self, row_index: int, col_index: int) -> bool:
+        up = self.get_height(row_index - 1, col_index)
+        down = self.get_height(row_index + 1, col_index)
+        left = self.get_height(row_index, col_index - 1)
+        right = self.get_height(row_index, col_index + 1)
+        height = self.get_height(row_index, col_index)
+
+        return height < up and height < down and height < left and height < right
+
+    def get_height(self, row_index, col_index) -> Union[int, float]:
+        try:
+            return self._heights[row_index][col_index]
+        except IndexError:
+            return float("inf")
+
+
+class Solution(AdventOfCodeSolutionBase):
+    def data_parser(self):
+        return lambda x: tuple(int(v) for v in x)
+
+    def part1(self):
+        height_map = HeightMap(self.input_data[:])
+        return height_map.calculate_risk_level()
+
+    def part2(self):
+        return None
