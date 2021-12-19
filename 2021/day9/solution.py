32c4ab7dfdfba518931626ff02a7e3f9ccf14ea5
liftnhoff <45518408+liftnhoff@users.noreply.github.com>
2021-12-18 20:19:37 -0500

    day 9 part 2

--- 2021/day9/solution.py
+++ 2021/day9/solution.py
@@ -1,3 +1,4 @@
+from collections import deque
 from dataclasses import dataclass
 from typing import Generic, TypeVar, Union
 
@@ -10,6 +11,9 @@ PointType = TypeVar("PointType")
 class Point(Generic[PointType]):
     row_index: int
     col_index: int
+
+    def __hash__(self):
+        return hash((self.row_index, self.col_index))
 
     def up(self) -> PointType:
         return Point(self.row_index - 1, self.col_index)
@@ -24,18 +28,27 @@ class Point(Generic[PointType]):
         return Point(self.row_index, self.col_index + 1)
 
 
-class HeightMap:
-    _RISK_CONSTANT = 1
+@dataclass
+class Basin:
+    points: list[PointType]
 
+    @property
+    def size(self) -> int:
+        return len(self.points)
+
+
+class HeightMap:
     def __init__(self, heights: tuple[tuple[int]]):
         self._heights = heights
 
         self._local_minima: list[Point] = []
+        self._basins: list[Basin] = []
 
     def calculate_risk_level(self) -> int:
+        risk_constant = 1
         risk = 0
         for point in self.local_minima:
-            risk += self._RISK_CONSTANT + self.get_height(point)
+            risk += risk_constant + self.get_height(point)
 
         return risk
 
@@ -52,7 +65,7 @@ class HeightMap:
 
         return self._local_minima
 
-    def _is_point_local_minimum(self, point: PointType) -> bool:
+    def _is_point_local_minimum(self, point: Point) -> bool:
         up = self.get_height(point.up())
         down = self.get_height(point.down())
         left = self.get_height(point.left())
@@ -61,7 +74,7 @@ class HeightMap:
 
         return height < up and height < down and height < left and height < right
 
-    def get_height(self, point: PointType) -> Union[int, float]:
+    def get_height(self, point: Point) -> Union[int, float]:
         if (
             point.row_index < 0
             or point.row_index >= len(self._heights)
@@ -71,6 +84,39 @@ class HeightMap:
             return float("inf")
         else:
             return self._heights[point.row_index][point.col_index]
+
+    @property
+    def basins(self) -> list[Basin]:
+        if self._basins:
+            return self._basins
+
+        for point in self.local_minima:
+            points = self._get_points_in_basin(point)
+            self._basins.append(Basin(points))
+
+        return self._basins
+
+    def _get_points_in_basin(self, starting_point: Point) -> list[Point]:
+        to_check = deque()
+        to_check.append(starting_point)
+        points = set()
+
+        max_basin_height = 8
+        while len(to_check) > 0:
+            point = to_check.popleft()
+            height = self.get_height(point)
+            if height <= max_basin_height:
+                points.add(point)
+                for adjacent_point in (
+                    point.up(),
+                    point.down(),
+                    point.left(),
+                    point.right(),
+                ):
+                    if adjacent_point not in points:
+                        to_check.append(adjacent_point)
+
+        return points
 
 
 class Solution(AdventOfCodeSolutionBase):
@@ -82,4 +128,6 @@ class Solution(AdventOfCodeSolutionBase):
         return height_map.calculate_risk_level()
 
     def part2(self):
-        return None
+        height_map = HeightMap(self.input_data[:])
+        basin_sizes = sorted((basin.size for basin in height_map.basins), reverse=True)
+        return basin_sizes[0] * basin_sizes[1] * basin_sizes[2]
