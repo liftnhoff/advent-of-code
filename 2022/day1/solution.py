01ce44112ebe1883190b15c4abf5bc2fcc88aea8
liftnhoff <45518408+liftnhoff@users.noreply.github.com>
2022-12-03 09:57:47 -0500

    d1p1

--- 2022/day1/solution.py
+++ 2022/day1/solution.py
@@ -3,10 +3,21 @@ from base.solution import AdventOfCodeSolutionBase
 
 class Solution(AdventOfCodeSolutionBase):
     def data_parser(self):
-        return lambda x: x
+        return lambda x: int(x) if x else None
 
     def part1(self):
-        return None
+        calorie_counts = []
+        current_pack = []
+        for calories in self.input_data:
+            if calories is None:
+                calorie_counts.append(sum(current_pack))
+                current_pack = []
+            else:
+                current_pack.append(calories)
+
+        calorie_counts.append(sum(current_pack))
+
+        return max(calorie_counts)
 
     def part2(self):
         return None
