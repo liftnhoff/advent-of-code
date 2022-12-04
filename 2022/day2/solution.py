11eb29996259c0c41f46c3f1f07d9214e3a47e03
liftnhoff <45518408+liftnhoff@users.noreply.github.com>
2022-12-04 08:57:09 -0500

    d2p2

--- 2022/day2/solution.py
+++ 2022/day2/solution.py
@@ -17,29 +17,7 @@ class Play(IntEnum):
 
 class Solution(AdventOfCodeSolutionBase):
     def data_parser(self):
-        def parser(x: str) -> tuple[Play, Play]:
-            you, me = x.split(" ")
-            if you == "A":
-                your_play = Play.ROCK
-            elif you == "B":
-                your_play = Play.PAPER
-            elif you == "C":
-                your_play = Play.SCISSORS
-            else:
-                raise RuntimeError(f"Invalid input for your play '{you}'")
-
-            if me == "X":
-                my_play = Play.ROCK
-            elif me == "Y":
-                my_play = Play.PAPER
-            elif me == "Z":
-                my_play = Play.SCISSORS
-            else:
-                raise RuntimeError(f"Invalid input for my play '{me}'")
-
-            return your_play, my_play
-
-        return parser
+        return lambda x: x
 
     def part1(self) -> int:
         my_score_matrix = {
@@ -55,10 +33,72 @@ class Solution(AdventOfCodeSolutionBase):
         }
 
         my_total_score = 0
-        for your_play, my_play in self.input_data:
+        for line in self.input_data:
+            your_play, my_play = self._opponent_and_me_interpretation(line)
             my_total_score += my_score_matrix[(your_play, my_play)]
 
         return my_total_score
 
-    def part2(self):
-        return None
+    def _opponent_and_me_interpretation(self, line: str):
+        you, me = line.split(" ")
+        if you == "A":
+            your_play = Play.ROCK
+        elif you == "B":
+            your_play = Play.PAPER
+        elif you == "C":
+            your_play = Play.SCISSORS
+        else:
+            raise RuntimeError(f"Invalid input for opponent play '{you}'")
+
+        if me == "X":
+            my_play = Play.ROCK
+        elif me == "Y":
+            my_play = Play.PAPER
+        elif me == "Z":
+            my_play = Play.SCISSORS
+        else:
+            raise RuntimeError(f"Invalid input for my play '{me}'")
+
+        return your_play, my_play
+
+    def part2(self) -> int:
+        my_score_matrix = {
+            (Play.ROCK, GamePoints.LOSS): GamePoints.LOSS + Play.SCISSORS,
+            (Play.ROCK, GamePoints.DRAW): GamePoints.DRAW + Play.ROCK,
+            (Play.ROCK, GamePoints.WIN): GamePoints.WIN + Play.PAPER,
+            (Play.PAPER, GamePoints.LOSS): GamePoints.LOSS + Play.ROCK,
+            (Play.PAPER, GamePoints.DRAW): GamePoints.DRAW + Play.PAPER,
+            (Play.PAPER, GamePoints.WIN): GamePoints.WIN + Play.SCISSORS,
+            (Play.SCISSORS, GamePoints.LOSS): GamePoints.LOSS + Play.PAPER,
+            (Play.SCISSORS, GamePoints.DRAW): GamePoints.DRAW + Play.SCISSORS,
+            (Play.SCISSORS, GamePoints.WIN): GamePoints.WIN + Play.ROCK,
+        }
+
+        my_total_score = 0
+        for line in self.input_data:
+            your_play, outcome = self._opponent_and_outcome_interpretation(line)
+            my_total_score += my_score_matrix[(your_play, outcome)]
+
+        return my_total_score
+
+    def _opponent_and_outcome_interpretation(self, line: str):
+        you, outcome_code = line.split(" ")
+        if you == "A":
+            your_play = Play.ROCK
+        elif you == "B":
+            your_play = Play.PAPER
+        elif you == "C":
+            your_play = Play.SCISSORS
+        else:
+            raise RuntimeError(f"Invalid input for opponent play '{you}'")
+
+        if outcome_code == "X":
+            outcome = GamePoints.LOSS
+        elif outcome_code == "Y":
+            outcome = GamePoints.DRAW
+        elif outcome_code == "Z":
+            outcome = GamePoints.WIN
+        else:
+            raise RuntimeError(f"Invalid input for outcome '{outcome_code}'")
+
+        return your_play, outcome
