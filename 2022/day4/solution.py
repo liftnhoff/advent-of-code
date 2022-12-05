977579d773fb6be875069bc1046bc747c746b7e6
liftnhoff <45518408+liftnhoff@users.noreply.github.com>
2022-12-05 06:31:32 -0500

    d4p1

--- 2022/day4/solution.py
+++ 2022/day4/solution.py
@@ -1,12 +1,36 @@
+import re
+from dataclasses import dataclass
+
 from base.solution import AdventOfCodeSolutionBase
+
+
+@dataclass
+class SectionRange:
+    start: int
+    end: int
 
 
 class Solution(AdventOfCodeSolutionBase):
     def data_parser(self):
-        return lambda x: x
+        def parser(line: str) -> tuple[SectionRange, SectionRange]:
+            pattern = re.compile(r"^(\d+)-(\d+),(\d+)-(\d+)$")
+            if match := pattern.match(line):
+                return (
+                    SectionRange(int(match.group(1)), int(match.group(2))),
+                    SectionRange(int(match.group(3)), int(match.group(4))),
+                )
+
+        return parser
 
     def part1(self):
-        return None
+        contained_pairs = 0
+        for r1, r2 in self.input_data:
+            if r1.start >= r2.start and r1.end <= r2.end:
+                contained_pairs += 1
+            elif r2.start >= r1.start and r2.end <= r1.end:
+                contained_pairs += 1
+
+        return contained_pairs
 
     def part2(self):
         return None
