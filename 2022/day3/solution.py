3401554c1d78c99c74077051773e0f97e072dea5
morri <morrisaj18@gmail.com>
2022-12-06 18:37:57 -0700

    day 1 start

---
+++ 2022/day3/solution.py
@@ -0,0 +1,13 @@
+from base.soultuion import AdventOfCodeSolutionBase
+
+
+class Solution(AdventOfCodeSolutionBase):
+    def data_pasrser(self):
+        return lambda x: x
+        
+    def part1(self):
+        return None
+    
+    def part2(self):
+        return None
+    
