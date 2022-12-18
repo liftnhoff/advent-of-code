47590fdfcb8400b162aff76690031fbc3a0c533f
liftnhoff <45518408+liftnhoff@users.noreply.github.com>
2022-12-18 05:55:47 -0500

    d7p1

---
+++ 2022/day7/solution.py
@@ -0,0 +1,43 @@
+import re
+from collections import defaultdict
+
+from base.solution import AdventOfCodeSolutionBase
+
+
+class Solution(AdventOfCodeSolutionBase):
+    def data_parser(self):
+        return lambda x: x
+
+    def part1(self):
+        directory_sizes = self._get_directory_sizes()
+
+        answer = 0
+        for directory, size in directory_sizes.items():
+            if size <= 100_000:
+                answer += size
+
+        return answer
+
+    def _get_directory_sizes(self) -> dict[str, int]:
+        cd_pattern = re.compile(r"^\$ cd (\S+)$")
+        file_pattern = re.compile(r"^(\d+) (\S+)$")
+
+        current_directories = []
+        directory_sizes = defaultdict(int)
+        for line in self.input_data:
+            if match := cd_pattern.match(line):
+                directory = match.group(1)
+                if directory == "..":
+                    current_directories.pop()
+                else:
+                    current_directories.append(directory)
+            elif match := file_pattern.match(line):
+                size = int(match.group(1))
+                for index in range(1, len(current_directories) + 1):
+                    path = "/".join(current_directories[:index])
+                    directory_sizes[path] += size
+
+        return directory_sizes
+
+    def part2(self):
+        return None
