ca934933c2543bc9e3e81cb029799ffd910fe479
liftnhoff <45518408+liftnhoff@users.noreply.github.com>
2023-12-01 07:04:11 -0500

    day 1 2023

--- 2023/day1/solution.py
+++ 2023/day1/solution.py
@@ -1,3 +1,5 @@
+import re
+
 from base.solution import AdventOfCodeSolutionBase
 
 
@@ -6,7 +8,75 @@ class Solution(AdventOfCodeSolutionBase):
         return lambda x: x
 
     def part1(self):
-        return None
+        # Note that the test input is different between days, this won't work with the
+        # current test input.
+        numbers = []
+        for row in self.input_data:
+            row_numbers = []
+            for value in row:
+                try:
+                    number = int(value)
+                except ValueError:
+                    continue
+                row_numbers.append(number)
+            numbers.append(int(f"{row_numbers[0]}{row_numbers[-1]}"))
+
+        return sum(numbers)
 
     def part2(self):
-        return None
+        # build the ridiculous smashed number strings like "eighthree"
+        number_lookup = {}
+        numbers = (
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine",
+        )
+        for xi, xx in enumerate(numbers, start=1):
+            for yi, yy in enumerate(numbers, start=1):
+                smashed_number = xx + yy[1:]
+                number_lookup[smashed_number] = f"{xi}{yi}"
+
+        number_lookup.update(
+            {
+                "1": "1",
+                "2": "2",
+                "3": "3",
+                "4": "4",
+                "5": "5",
+                "6": "6",
+                "7": "7",
+                "8": "8",
+                "9": "9",
+                "one": "1",
+                "two": "2",
+                "three": "3",
+                "four": "4",
+                "five": "5",
+                "six": "6",
+                "seven": "7",
+                "eight": "8",
+                "nine": "9",
+            }
+        )
+
+        regex = re.compile(f"({'|'.join(number_lookup.keys())})")
+        numbers = []
+        for row in self.input_data:
+            row_numbers = regex.findall(row)
+            parsed_numbers = []
+            for value in row_numbers:
+                number = number_lookup[value]
+                if len(number) > 1:
+                    parsed_numbers.extend(number)
+                else:
+                    parsed_numbers.append(number)
+
+            numbers.append(int(parsed_numbers[0] + parsed_numbers[-1]))
+
+        return sum(numbers)
