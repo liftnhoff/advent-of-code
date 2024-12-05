1575345360d2d49c67bc5da7a53e9bc911c46b61
liftnhoff <45518408+liftnhoff@users.noreply.github.com>
2024-12-05 06:01:38 -0500

    2024d5p2

--- 2024/day5/solution.py
+++ 2024/day5/solution.py
@@ -8,18 +8,10 @@ class Solution(AdventOfCodeSolutionBase):
         return lambda x: x
 
     def part1(self):
-        before_rules_by_pn, after_rules_by_pn, updates = self.get_rules_and_updates()
+        before_rules_by_pn, updates = self.get_rules_and_updates()
         valid_updates = []
         for update in updates:
-            is_valid = True
-            pn_seen = set()
-            for pn in update:
-                if pn_seen.intersection(before_rules_by_pn[pn]):
-                    is_valid = False
-                    break
-                pn_seen.add(pn)
-
-            if is_valid:
+            if self.is_order_valid(before_rules_by_pn, update):
                 valid_updates.append(update)
 
         score = 0
@@ -29,18 +21,52 @@ class Solution(AdventOfCodeSolutionBase):
         return score
 
     def part2(self):
-        return None
+        before_rules_by_pn, updates = self.get_rules_and_updates()
+        invalid_updates = []
+        for update in updates:
+            if not self.is_order_valid(before_rules_by_pn, update):
+                invalid_updates.append(update)
+
+        fixed_updates = []
+        for update in invalid_updates:
+            new_update = []
+            for pn in update:
+                new_update.append(pn)
+                if not self.is_order_valid(before_rules_by_pn, new_update):
+                    new_update.pop()
+                    for index in range(len(new_update)):
+                        new_update.insert(index, pn)
+                        if self.is_order_valid(before_rules_by_pn, new_update):
+                            break
+                        else:
+                            new_update.pop(index)
+            fixed_updates.append(new_update)
+
+        score = 0
+        for update in fixed_updates:
+            score += update[int(len(update) / 2)]
+
+        return score
 
     def get_rules_and_updates(self):
         before_rules_by_pn = defaultdict(set)
-        after_rules_by_pn = defaultdict(set)
         updates = []
         for line in self.input_data:
             if "|" in line:
                 rule = tuple(int(v) for v in line.split("|"))
                 before_rules_by_pn[rule[0]].add(rule[1])
-                after_rules_by_pn[rule[1]].add(rule[0])
             elif "," in line:
                 updates.append(tuple(int(v) for v in line.split(",")))
 
-        return before_rules_by_pn, after_rules_by_pn, updates
+        return before_rules_by_pn, updates
+
+    def is_order_valid(self, before_rules_by_pn, update):
+        is_valid = True
+        pn_seen = set()
+        for pn in update:
+            if pn_seen.intersection(before_rules_by_pn[pn]):
+                is_valid = False
+                break
+            pn_seen.add(pn)
+
+        return is_valid
