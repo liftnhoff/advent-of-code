a20545f2b56b2057f5f54d35adc9336e08689370
liftnhoff <45518408+liftnhoff@users.noreply.github.com>
2024-12-08 05:09:10 -0500

    2024d7

--- 2024/day7/solution.py
+++ 2024/day7/solution.py
@@ -1,5 +1,7 @@
 import itertools
 
+import tqdm
+
 from base.solution import AdventOfCodeSolutionBase
 
 
@@ -15,19 +17,29 @@ class Solution(AdventOfCodeSolutionBase):
 
     def part1(self):
         operators = (add, mul)
-        for ans, inputs in self.input_data:
+        return self._check_operators(operators)
+
+    def _check_operators(self, operators):
+        total = 0
+        for ans, inputs in tqdm.tqdm(self.input_data):
             operator_permutations = list(
                 itertools.product(operators, repeat=len(inputs) - 1)
             )
 
-            # for ops in operator_permutations:
-            #     current = 0
-            #     for index, op in enumerate(ops):
+            for ops in operator_permutations:
+                current = inputs[0]
+                for index, op in enumerate(ops):
+                    current = op(current, inputs[index + 1])
 
-        return None
+                if current == ans:
+                    total += current
+                    break
+
+        return total
 
     def part2(self):
-        return None
+        operators = (add, mul, cat)
+        return self._check_operators(operators)
 
 
 def add(a, b):
@@ -36,3 +48,7 @@ def add(a, b):
 
 def mul(a, b):
     return a * b
+
+
+def cat(a, b):
+    return int(f"{a}{b}")
