15dbca8314f6260452c1dece7f25e6c22efeeec7
liftnhoff <45518408+liftnhoff@users.noreply.github.com>
2025-12-23 20:35:01 -0500

    2025 d4 p1

--- 2025/src/Day04.java
+++ 2025/src/Day04.java
@@ -1,36 +1,67 @@
-//import java.nio.file.Files;
-//import java.nio.file.Paths;
-//
-//public class Day04 {
-//    public static void main(String[] args) throws Exception {
-//        var lines = Files.readAllLines(Paths.get("inputs/day03.txt"));
-//        System.out.println("Part 1: " + part1(lines));
-//        System.out.println("Part 2: " + part2(lines));
-//    }
-//
-//    static long part1(java.util.List<String> lines) {
-//        return 0;
-//    }
-//
-//    static long part2(java.util.List<String> lines) {
-//        return 0;
-//    }
-//}
-//
-//
-//class PaperMap {
-//
-//    private char[][] positions = new char[][];
-//
-//    void PaperMap() {
-//
-//    }
-//
-//    public char[][] getPositions() {
-//        return positions;
-//    }
-//
-//    public void setPositions(char[][] positions) {
-//        this.positions = positions;
-//    }
-//}
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day04 {
+    public static void main(String[] args) throws Exception {
+        var lines = Files.readAllLines(Paths.get("2025/inputs/day04.txt"));
+
+        List<List<Character>> charGrid = lines.stream()
+                .map(str -> str.chars()
+                        .mapToObj(c -> (char) c)
+                        .collect(Collectors.toList()))
+                .toList();
+
+        System.out.println("Part 1: " + part1(charGrid));
+        System.out.println("Part 2: " + part2(charGrid));
+    }
+
+    static long part1(List<List<Character>> charGrid) {
+        int rowCount = charGrid.size();
+        int colCount = charGrid.getFirst().size();
+
+        long moveableCount = 0;
+        for (int rowIndex = 0; rowIndex < rowCount; rowIndex++) {
+            for (int colIndex = 0; colIndex < colCount; colIndex++) {
+                if (charGrid.get(rowIndex).get(colIndex) == '.') {
+                    continue;
+                }
+
+                int freeSpaces = 0;
+                for (int ro = -1; ro < 2; ro++) {
+                    for (int co = -1; co < 2; co++) {
+                        int ri = rowIndex + ro;
+                        int ci = colIndex + co;
+//                        System.out.printf("%3d %3d %3d\n", ri, ci, freeSpaces);
+                        if (ro == 0 && co == 0) {
+                            continue;
+                        }
+                        if (ri < 0 || ri >= rowCount) {
+                            freeSpaces += 1;
+                            continue;
+                        }
+                        if (ci < 0 || ci >= colCount) {
+                            freeSpaces += 1;
+                            continue;
+                        }
+
+                        if (charGrid.get(ri).get(ci) == '.') {
+                            freeSpaces += 1;
+                        }
+                    }
+                }
+
+                if (freeSpaces >= 5) {
+                    moveableCount += 1;
+                }
+            }
+        }
+
+        return moveableCount;
+    }
+
+    static long part2(List<List<Character>> charGrid) {
+        return 0;
+    }
+}
