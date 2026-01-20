23964c40bf39c6587e630700e71fb5a50bb80eeb
liftnhoff <45518408+liftnhoff@users.noreply.github.com>
2026-01-20 05:47:25 -0500

    2019 d1

--- 2019/day1/solution.py
+++ 2019/day1/solution.py
@@ -3,10 +3,19 @@ from base.solution import AdventOfCodeSolutionBase
 
 class Solution(AdventOfCodeSolutionBase):
     def data_parser(self):
-        return lambda x: x
+        return lambda x: int(x)
 
     def part1(self):
-        return None
+        total = 0
+        for mass in self.input_data:
+            total += mass // 3 - 2
+        return total
 
     def part2(self):
-        return None
+        total = 0
+        for mass in self.input_data:
+            mass = mass // 3 - 2
+            while mass > 0:
+                total += mass
+                mass = mass // 3 - 2
+        return total
