6842c4fce262fa587574c9cc0b291c236d45d99d
liftnhoff <45518408+liftnhoff@users.noreply.github.com>
2026-02-09 05:49:07 -0500

    2025 d1 ts

--- 2025-ts/day1/day1.ts
+++ 2025-ts/day1/day1.ts
@@ -1,21 +1,84 @@
 import { readFileSync } from "fs";
 
 function main() {
-  let lines = loadInput("input.txt");
-  console.log("Part 1: " + part1(lines));
-  console.log("Part 2: " + part2(lines));
+    let lines = loadInput("input.txt");
+    // let lines = loadInput("test-input.txt");
+    console.log("Part 1: " + part1(lines));
+    console.log("Part 2: " + part2(lines));
 }
 
 function loadInput(filepath: string): string[] {
-  return readFileSync(filepath, "utf-8").split("\n");
+    return readFileSync(filepath, "utf-8").split("\n");
 }
 
 function part1(lines: string[]): number {
-  return 0;
+    const dialMax: number = 99;
+    let position: number = 50;
+    let zeroCount: number = 0;
+
+    for (const line of lines) {
+        if (line.length == 0) {
+            continue;
+        }
+
+        let direction = line[0];
+        let amount: number = Number(line.substring(1));
+        if (direction == "R") {
+            position += amount;
+            while (position > dialMax) {
+                position -= dialMax + 1;
+            }
+        } else {
+            position -= amount;
+            while (position < 0) {
+                position += dialMax + 1;
+            }
+        }
+
+        if (position == 0) {
+            zeroCount += 1;
+        }
+    }
+
+    return zeroCount;
 }
 
 function part2(lines: string[]): number {
-  return 0;
+    const dialMax: number = 99;
+    let position: number = 50;
+    let zeroCount: number = 0;
+
+    for (const line of lines) {
+        if (line.length == 0) {
+            continue;
+        }
+
+        let direction = line[0];
+        let amount: number = Number(line.substring(1));
+        if (direction == "R") {
+            for (let index = 0; index < amount; index++) {
+                position += 1;
+                if (position > dialMax) {
+                    position -= dialMax + 1;
+                }
+                if (position == 0) {
+                    zeroCount += 1;
+                }
+            }
+        } else {
+            for (let index = 0; index < amount; index++) {
+                position -= 1;
+                if (position < 0) {
+                    position += dialMax + 1;
+                }
+                if (position == 0) {
+                    zeroCount += 1;
+                }
+            }
+        }
+    }
+
+    return zeroCount;
 }
 
 main();
